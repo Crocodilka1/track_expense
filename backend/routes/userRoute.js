c5afdf7bebd6010cd2c08858302c751936f36fba
crocodilka1 <crocodilka1@gmail.com>
2026-09-17 19:01:05 +0300

    init project

---
+++ backend/routes/userRoute.js
@@ -0,0 +1,14 @@
+import express from "express";
+import { getCurrentUser, loginUser, registerUser, updatePassword, updateProfile } from "../controllers/userController.js";
+import authMiddleware from "../middleware/auth.js";
+
+const router = express.Router();
+
+router.post('/register', registerUser);
+router.post('/login', loginUser);
+
+router.get('/me', authMiddleware, getCurrentUser);
+router.put('/profile', authMiddleware, updateProfile);
+router.put('/password', authMiddleware, updatePassword);
+
+export default router;
---
+++ backend/server.js
@@ -0,0 +1,34 @@
+import express from "express"
+import cors from "cors"
+import { config } from "dotenv";
+import { connectDB } from "./config/db.js";
+import userRouter from './routes/userRoute.js';
+
+config();
+
+const app = express();
+
+// MIDDLEWARES
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
+//DB
+connectDB();
+
+
+//ROUTES
+app.use('/api/user', userRouter);
+
+
+app.get('/', (req, res) => {
+    res.send("API IS WORKING")
+})
+
+
+
+
+app.listen(process.env.PORT || 5001, () => {
+    console.log(`Application running on http://localhost:${process.env.PORT}`)
+})
